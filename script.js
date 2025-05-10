ffb528a2d83b6a0aaec473fc57174a085787ae1f
ClóvisCordeiroMorici <illusion350@hotmail.com>
2025-05-10 19:59:53 -0300

    Projeto NIKE

---
+++ script.js
@@ -0,0 +1,11 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".tenis1")
+
+
+function mudarvisual(cor, imagem) {
+    body.style.background = cor;
+    tenis.src = imagem;
+
+}
+
+
